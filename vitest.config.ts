cc3688d5da6b334bdd3ab6aa30e700074660cb5d
RanHolly <miiabigloli@gmail.com>
2024-08-31 21:21:55 +0300

    Изменила конфигурации

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@type": "/types",
+      "@client": "/srcTs/client",
+      "@base": "/srcTs/base",
+      "@util": "/srcTs/util",
+      "@utils_": "/srcTs/utils.ts",
+      "@log": "/srcTs/log",
+      "@handlers": "/srcTs/handlers"
+    }
+  },
   test: {
     globals: true,
     environment: 'node',
