ebf9dd6c0977a137e8ac9e3a5744dfdea74e88f9
RanHolly <miiabigloli@gmail.com>
2024-09-02 22:27:23 +0300

    перенесла в index, так как всё равно на index была ссылка

--- srcTs/database/index.ts
+++ srcTs/database/index.ts
@@ -1,1 +1,12 @@
-export * from "./drizzle";
+import { Log } from "@log";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { schema } from "./schemas";
+
+const pgPool = new Pool({
+  connectionString: process.env.DATABASE_URL ?? `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}/${process.env.DB_NAME}?schema=public`
+});
+
+new Log({ text: "Подключение к базе данных успешно установлено", type: "info", categories: ["global", "pg"] });
+
+export const db = drizzle(pgPool, { schema });
